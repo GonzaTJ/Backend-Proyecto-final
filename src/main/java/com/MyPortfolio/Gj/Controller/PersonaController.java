11a3bbdf14ab4a263e70a2851a62493d44fb7608
GonzaTJ <95258624+GonzaTJ@users.noreply.github.com>
2022-09-20 15:50:31 -0300

    Resub

--- src/main/java/com/MyPortfolio/Gj/Controller/PersonaController.java
+++ src/main/java/com/MyPortfolio/Gj/Controller/PersonaController.java
@@ -3,8 +3,9 @@ package com.MyPortfolio.Gj.Controller;
 import com.MyPortfolio.Gj.Entity.Persona;
 import com.MyPortfolio.Gj.Interface.IPersonaService;
 import java.util.List;
-import static org.hibernate.criterion.Projections.id;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     
@@ -23,19 +25,22 @@ public class PersonaController {
         return ipersonaService.getPersona();
     }
     
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/personas/crear")
     public String createPersona(@RequestBody Persona persona){
         ipersonaService.savePersona(persona);
         return "La persona fue creada corrrectamente";
     }
     
-    @DeleteMapping("/personas/borrar/(id)")
+    @PreAuthorize("hasRole('ADMIN')")
+    @DeleteMapping("/personas/borrar/{id}")
     public  String deletePersona(@PathVariable Long id){
         ipersonaService.deletePersona(id);
         return "La persona fue eliminada";
     }
     
-    @PutMapping("/personas/editar/(id)")
+    @PreAuthorize("hasRole('ADMIN')")
+    @PutMapping("/personas/editar/{id}")
     public Persona editPersona(@PathVariable Long id,
                                @RequestParam("nombre")String nuevoNombre,
                                @RequestParam("apellido")String nuevoApellido,
@@ -48,5 +53,10 @@ public class PersonaController {
         
         ipersonaService.savePersona(persona);
         return persona;
+    }
+    
+    @GetMapping("/personas/traer/perfil")
+    public Persona findPersona(){
+        return ipersonaService.findPersona((long)1);
     }
 }
